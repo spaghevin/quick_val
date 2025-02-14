be114671ac6a4f9b54731fb319f03cad9e527900
spaghevin <spaghevin@gmail.com>
2025-02-13 22:25:19 -0800

    more pages

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById("main_submit").addEventListener("click", function() {
+    location.href = "question.html"; // Redirects to static.html
+});
+
+
+// document.getElementById("left_kayla").onclick = function () {
+//     location.href = "love.html";
+// };
+
+// document.getElementById("right_kayla").onclick = function () {
+//     location.href = "love.html";
+// };
